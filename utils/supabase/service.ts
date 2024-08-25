d7d88f77fdc0898291b306799888ddeb838215d3
karam14 <krm-011@hotmail.com>
2024-08-25 12:49:55 +0200

    hotfix: fix: supabase service

--- utils/supabase/service.ts
+++ utils/supabase/service.ts
@@ -2,6 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const getServiceSupabase = () => createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
-!
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
